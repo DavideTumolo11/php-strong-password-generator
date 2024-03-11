17b5fe9f744405c903cbf8ec453856e6ec70748b
DavideTumolo11 <davidetumolo11@gmail.com>
2024-03-11 16:25:16 +0100

    generate_password

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include "/MAMP/htdocs/php-strong-password-generator/function.php";
+if (isset($_GET['length'])) {
+    $length = $_GET['length'];
+    echo generatePassword($length);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+<body>
+
+    <div>
+        <form method="get">
+            <label for="length">Lunghezza della password:</label><br>
+            <input type="number" id="length" name="length" min="1"><br>
+            <input type="submit" value="Genera">
+        </form>
+    </div>
+
+
+</body>
+
+</html>
